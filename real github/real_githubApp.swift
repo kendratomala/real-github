da301ac81bb8a448d4c888d4c5972d6c8f912304
kendra <kendratomala@gmail.com>
2023-06-12 08:07:17 -0700

    Initial Commit

---
+++ real github/real_githubApp.swift
@@ -0,0 +1,17 @@
+//
+//  real_githubApp.swift
+//  real github
+//
+//  Created by Kendra Tomala on 6/12/23.
+//
+
+import SwiftUI
+
+@main
+struct real_githubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
